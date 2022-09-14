42e0731ae12a886eee42ac1aa18ef803741331e5
Teemu Turunen <teemu.turunen@koulutus.kynet.fi>
2022-09-14 12:36:35 +0300

    Add project files.

---
+++ Polttoaine/Ajoneuvo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polttoaine
+{
+    class Ajoneuvo
+    {
+        // property => data
+
+        public double Polttoaineenmaara { get; set; }
+        public double Polttoainetankki { get; set; }
+        // lisää keskikulutus
+        public double Keskikulutus { get; set; }
+
+        public string Nimi { get; set; }
+
+        // nimi
+        // merkki
+        // malli
+        // id 0..1...2..
+
+        // method / function => toiminnallisuus
+
+        // static => voidaan käyttää ilman, että luokasta
+        // on luotu objekti
+
+        public Ajoneuvo(string nimi) // constructor. kun luodaan luokasta objekti
+        {
+            Polttoaineenmaara = 5; // oletusarvot
+            Polttoainetankki = 50;
+            Keskikulutus = 7.8;
+            Nimi = nimi;
+        }
+
+        public void Tankkaus()
+        {
+            double polttoaineenHinta = 1.88; // euroa
+
+            // tankin tilavuus - tämän hetken polttoaine
+            // = puuttuva polttoaine
+
+            double puuttuvaPolttoaine = Polttoainetankki -
+                Polttoaineenmaara;
+            Console.WriteLine($"Polttoainetta tankattu: " +
+                $"{puuttuvaPolttoaine}");
+            Polttoaineenmaara = Polttoainetankki;
+            // määritellään polttoaineen määräksi sama kuin
+            // polttoainetankin maksimi tilavuus
+
+            // Lasketaan tankatun polttoaineen hinta ja
+            // ilmoitetaan käyttäjälle
+
+            double hinta = puuttuvaPolttoaine * polttoaineenHinta;
+            Console.WriteLine($"Tankkauksen hinta: {hinta}");
+
+        } // Tankkaus() päättyy
+
+        public void Ajo(double kilometrit)
+        {
+            // laske matkalla kuluva polttoaine
+            double kulunutPolttoaine = kilometrit / 100 * Keskikulutus;
+            // kaava: kilometrit / 100 * keskikulutus (L/100km)
+            // päivitä polttoaineenmäärä ajoneuvossa
+            Polttoaineenmaara = Polttoaineenmaara - kulunutPolttoaine;
+            // ilmoita käyttäjälle kulunut polttoaine
+            Console.WriteLine($"Polttoainetta kului: {kulunutPolttoaine}");
+            Console.WriteLine($"Polttoainetta on jäljellä: {Polttoaineenmaara}");
+        }
+    }
+}
---
+++ Polttoaine/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Polttoaine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Täällä suoritetaan sovellus
+            // käytetään ajoneuvoja
+
+            // luodaan "ajoneuvo"-luokasta objekti
+            // tarvitaan tietorakenne
+            // Lista, tai taulukko
+            List<Ajoneuvo> autot = new List<Ajoneuvo>();
+            autot.Add(new Ajoneuvo());
+            // Ajoneuvo auto1 = new Ajoneuvo();
+            string input;
+            int aktiivinenAuto = 0; // listan indeksi
+
+            while (true)
+            {
+                Console.WriteLine("T <= Tankkaa ajoneuvo");
+                Console.WriteLine("A <= Aja ajoneuvolla");
+                Console.WriteLine("U <= Lisää ajoneuvo");
+                Console.Write("Valitse toiminto: ");
+                input = Console.ReadLine();
+
+                // rakenne, jolla suoritetaan käyttäjän haluama toiminto
+                switch (input)
+                {
+                    case "T":
+                        autot[aktiivinenAuto].Tankkaus();
+                        break;
+                    case "A":
+                        Console.WriteLine("Montako kilometriä ajetaan?");
+                        input = Console.ReadLine();
+                        autot[aktiivinenAuto].Ajo(Double.Parse(input));
+                        break;
+                    case "U":
+                        // kysytään käyttäjältä nimi
+                        autot.Add(new Ajoneuvo("Nimi")); // constructor parametri
+                        break;
+                    default: // käyttäjä ei syöttänyt hyväksyttävää arvoa
+                        break;
+                } // switch päättyy
+            } // while päättyy
+        } // main päättyy
+
+        // TEHTÄVIÄ
+        // 1. Näytä arvot kahden desimaalin tarkkuudella
+        // 2. lisää käyttäjälle mahdollisuus lopettaa sovelluksen suoritus
+        // 3. Lisää "Ajoneuvo"-luokkaan "AjetutKilometrit"-property,
+        //      jossa kerätään kaikkien ajojen kilometrit talteen.
+        //      Lisää käyttäjälle toiminto, joka näyttää ajetut kilometrit
+        // 4. Lisää käyttäjälle toiminto lisätä ajoneuvoja (uusi objekti)
+        // 5. Lisää ajoneuvolla Esim: "Nimi"-property
+        // 6. Käyttäjä pystyy vaihtamaan aktiivisen ajoneuvon
+
+    }
+}
